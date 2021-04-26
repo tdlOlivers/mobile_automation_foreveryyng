dec386eb7997a9304ec4e44bc22e501355087d58
Olivers Oskars Sīpols <olivers.sipols@testdevlab.com>
2021-04-26 09:13:45 +0300

    optimized paralel_runner

--- run_parallel.rb
+++ run_parallel.rb
@@ -8,14 +8,12 @@ require_relative 'mobile_runner'
 
 # Creating mobile runner object
 mobile_runner = MobileRunner.new
-# Reading predefined devices from yml
-devices = YAML.load_file('devices.yml')
-# Reading optins passed to run_parallel.rb
+# Reading options passed to run_parallel.rb
 options = {}
 OptionParser.new { |opts| opts.on('-t', '--tag NAME', 'Tag name') { |t| options[:tag] = t } }.parse!
-# Adding devices in yml file to mobile_runner
-devices['devices'].each { |device| mobile_runner.add_mobile_device name: device }
-
+# Reading and adding devices in yml file to mobile_runner
+YAML.load_file('devices.yml')['devices'].each { |device| mobile_runner.add_mobile_device name: device }
+# Executing tests
 mobile_runner.appium_starter
 mobile_runner.run_tests tag: options[:tag].nil? ? nil : options[:tag]
 mobile_runner.generate_report
